f1987e200ed1458628bbe36f76825f150cc0abc6
sokolovgit <sanok.sokolov928100@gmail.com>
2025-02-05 17:13:59 +0200

    chore(lab2): add html and ejs formatting

--- apps/lab2/eslint.config.mjs
+++ apps/lab2/eslint.config.mjs
@@ -3,4 +3,17 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   ignores: ['./dist', './node_modules'],
+
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+
+  rules: {
+
+  },
+
+  formatters: {
+    html: true,
+  },
 })
