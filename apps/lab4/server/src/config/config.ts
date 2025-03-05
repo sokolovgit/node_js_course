11b1ec22319fd88f460484d19af907cb77e7bc25
sokolovgit <sanok.sokolov928100@gmail.com>
2025-03-05 22:07:31 +0200

    feat(lab4): add db scripts and migratiosn

--- apps/lab4/server/src/config/config.ts
+++ apps/lab4/server/src/config/config.ts
@@ -2,10 +2,10 @@ export default () => ({
   port: parseInt(process.env.APP_PORT, 10) || 3000,
   frontendUrl: process.env.FRONTEND_URL,
   database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
   },
 })
