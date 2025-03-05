4782b482d61114fd3298f32cec8de706cd24eb83
sokolovgit <sanok.sokolov928100@gmail.com>
2025-03-05 22:30:27 +0200

    fix(lab4): use correct name for db username config

--- apps/lab4/server/src/database/database.module.ts
+++ apps/lab4/server/src/database/database.module.ts
@@ -1,17 +1,17 @@
 import { PG_CONNECTION } from "@/сonstant/pg-connection"
 import { Module, Global } from "@nestjs/common"
-import { ConfigModule, ConfigService } from "@nestjs/config"
+import { ConfigService } from "@nestjs/config"
 import { Pool } from "pg"
 
 @Global()
 @Module({
-  imports: [ConfigModule],
+  imports: [],
   providers: [
     {
       provide: PG_CONNECTION,
       useFactory: (configService: ConfigService) => {
         const pool = new Pool({
-          user: configService.get("DB_USER"),
+          user: configService.get("DB_USERNAME"),
           host: configService.get("DB_HOST"),
           database: configService.get("DB_NAME"),
           password: configService.get("DB_PASSWORD"),
