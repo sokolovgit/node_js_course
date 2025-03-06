bf5b0c9452fc3f3effe398725365c722eb69e6b1
sokolovgit <sanok.sokolov928100@gmail.com>
2025-03-06 09:38:24 +0200

    fix(lab4): remove bearer auth in docs

--- apps/lab4/server/src/main.ts
+++ apps/lab4/server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setTitle("Learning Management System API")
     .setDescription("The Learning Management System API description")
     .setVersion("1.0")
-    .addBearerAuth()
+    // .addBearerAuth()
     .build()
 
   const document = SwaggerModule.createDocument(app, options)
