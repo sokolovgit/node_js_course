4e2261f1a7cccb95d7499a86fac029f163ce76b9
sokolovgit <sanok.sokolov928100@gmail.com>
2025-04-29 23:50:37 +0300

    feat(server): configure typeorm

--- apps/lab5/server/src/app.module.ts
+++ apps/lab5/server/src/app.module.ts
@@ -1,12 +1,13 @@
 import config from "./config/config"
 
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
-import { DatabaseModule } from "./database/database.module"
-import { NotesDomainModule } from "./database/domains/notes/notes.domain-module"
-import { MembersDomainModule } from "./database/domains/members/members.domain-module"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 import { MembersModule } from "./domains/members/members.module"
 import { NotesModule } from "./domains/notes/notes.module"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import * as path from "path"
+import { NotesDomainModule } from "./database/domains/notes/notes.domain-module"
+import { MembersDomainModule } from "./database/domains/members/members.domain-module"
 
 @Module({
   imports: [
@@ -14,7 +15,27 @@ import { NotesModule } from "./domains/notes/notes.module"
       isGlobal: true,
       load: [config],
     }),
-    DatabaseModule,
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
+        entities: [
+          path.resolve(__dirname, "database/domains/**/*.entity.{js,ts}"),
+        ],
+        migrations: [path.resolve(__dirname, "database/migrations/*.{js,ts}")],
+        migrationsRun: false,
+        logging: true,
+        synchronize: true,
+      }),
+    }),
+
     NotesDomainModule,
     NotesModule,
     MembersDomainModule,
